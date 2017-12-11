73718bd2a2b3f0bf5e4536a493efe2b454907eff
ilyamoskalev <ilyamoskalev@mail.ru>
2017-12-11 03:20:58 +0300

    update main

--- src/main/java/hashtable/Main.java
+++ src/main/java/hashtable/Main.java
@@ -14,9 +14,13 @@ public class Main {
         final Path pathInput = Paths.get(args[0]);
         final Path pathOutput = Paths.get(args[1]);
         final ChainHashTable chainTable = new ChainHashTable();
+        final OpenAddressHashTableLP lpTable = new OpenAddressHashTableLP();
+        final OpenAddressHashTableDH dhTable = new OpenAddressHashTableDH();
         try {
             final List<String> inputData = Files.readAllLines(pathInput);
-            final List<String> outputData = new ArrayList<>();
+            final List<String> outputData1 = new ArrayList<>();
+            final List<String> outputData2 = new ArrayList<>();
+            final List<String> outputData3 = new ArrayList<>();
             for (String line : inputData) {
                 Integer value;
                 if(line.equals("print") || line.equals("min") || line.equals("max")){
@@ -25,42 +29,90 @@ public class Main {
                 switch (line.substring(0, line.indexOf(' '))) {
                     case "add":
                         value = Integer.parseInt(line.substring(line.lastIndexOf(' ') + 1));
-                        final int key = Integer.parseInt(line.substring(line.indexOf(' ') + 1, line.lastIndexOf(' ') - 1));
+                        int key = Integer.parseInt(line.substring(line.indexOf(' ') + 1, line.lastIndexOf(' ') - 1));
                         chainTable.add(key, value);
+                        lpTable.add(key, value);
+                        dhTable.add(key, value);
                         break;
                     case "delete":
-                        chainTable.delete(Integer.parseInt(line.substring(line.indexOf(' ') + 1)));
+                        key = Integer.parseInt(line.substring(line.indexOf(' ') + 1));
+                        chainTable.delete(key);
+                        lpTable.delete(key);
+                        dhTable.delete(key);
                         break;
                     case "search":
-                        value = chainTable.search(Integer.parseInt(line.substring(line.indexOf(' ') + 1)));
+                        key = Integer.parseInt(line.substring(line.indexOf(' ') + 1));
+                        value = chainTable.search(key);
                         if (value == null) {
-                            outputData.add("error");
+                            outputData1.add("error");
                         } else {
-                            outputData.add(value.toString());
+                            outputData1.add(value.toString());
+                        }
+                        value = lpTable.search(key);
+                        if (value == null) {
+                            outputData2.add("error");
+                        } else {
+                            outputData2.add(value.toString());
+                        }
+                        value = dhTable.search(key);
+                        if (value == null) {
+                            outputData3.add("error");
+                        } else {
+                            outputData3.add(value.toString());
                         }
                         break;
                     case "min":
                         value = chainTable.min();
                         if (value == null) {
-                            outputData.add("empty");
+                            outputData1.add("empty");
                         } else {
-                            outputData.add(value.toString());
+                            outputData1.add(value.toString());
+                        }
+                        value = lpTable.min();
+                        if (value == null) {
+                            outputData2.add("empty");
+                        } else {
+                            outputData3.add(value.toString());
+                        }
+                        value = dhTable.min();
+                        if (value == null) {
+                            outputData3.add("empty");
+                        } else {
+                            outputData3.add(value.toString());
                         }
                         break;
                     case "max":
                         value = chainTable.max();
                         if (value == null) {
-                            outputData.add("empty");
+                            outputData1.add("empty");
                         } else {
-                            outputData.add(value.toString());
+                            outputData1.add(value.toString());
+                        }
+                        value = lpTable.max();
+                        if (value == null) {
+                            outputData2.add("empty");
+                        } else {
+                            outputData3.add(value.toString());
+                        }
+                        value = dhTable.max();
+                        if (value == null) {
+                            outputData3.add("empty");
+                        } else {
+                            outputData3.add(value.toString());
                         }
                         break;
                     case "print":
-                        outputData.add(chainTable.print());
+                        outputData1.add(chainTable.print());
+                        outputData2.add(lpTable.print());
+                        outputData3.add(dhTable.print());
                         break;
                 }
             }
-            Files.write(pathOutput, outputData);
+            outputData1.add("");
+            outputData2.add("");
+            outputData1.addAll(outputData2);
+            outputData1.addAll(outputData3);
+            Files.write(pathOutput, outputData1);
         } catch (IOException e) {
             e.printStackTrace();
         }
